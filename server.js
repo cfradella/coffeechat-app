cc35aee4bf5e2e9bd73bf64f76d1d1923cfd2698
Christopher Fradella <chris@Christophers-MacBook-Pro.local>
2020-06-10 21:04:07 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const botName = 'System';
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', socket => {
+  console.log('new socket connection')
+
+  // Welcome current user
+  socket.emit('message', formatMessage(botName, 'Welcome to CoffeeChat!'))
+
+  // When a user connects
+  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat.'))
+
+  // When a user disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', formatMessage(botName, 'A user has left the chat.'))
+  })
+
+  // Listen for messages
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', formatMessage('You', msg))
+  })
+})
+
+const PORT = '3000' || process.env.PORT;
+
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
